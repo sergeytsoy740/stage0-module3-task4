4303c2adbe9c4168b9ecdcb31ab527f26cf8f8fa
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-10 14:46:45 +0600

    Module #3. Task #4. Primitives

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
