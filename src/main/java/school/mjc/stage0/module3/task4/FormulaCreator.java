6586869040274cfff0ebbfa5d92a9f2ea6c21af8
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-10 15:17:50 +0600

    Module #3. Task #4. Primitives (1)

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,5 +5,7 @@ public class FormulaCreator {
         int a = 6, b = 1;
 
         double res = (9 * a * a - 5 * b + 2 + a - 7) * ((double) (a + b - 4 * a * b) / 2);
+
+        System.out.println(res);
     }//
 }
